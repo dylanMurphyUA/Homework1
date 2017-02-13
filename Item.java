897dff8c06850e40e6d31dded475a97583170932
Unknown <dmurphy5@albany.edu>
2017-02-12 20:59:22 -0500

    examine()&main method

--- Item.java
+++ Item.java
@@ -7,20 +7,35 @@
  *
  */
 public class Item {
-	String name;
-	int weight;
-	public Item(String i, int j){
-		i = name;
-		j = weight;
-	}
-	public String getName(){
-		return name;
-	}
-	public int getWeight(){
-		return weight;
-	
-	}
-	
+ private String name;
+ private int weight;
+ public Item(String i, int j){
+  name = i;
+  weight = j;
+ }
+ public String getName(){
+  return name;
+ }
+ public int getWeight(){
+  return weight;
+ }
+ public void examine(){
+	 System.out.printf("Your %s weighs %dton(s)", name, weight);
+ }
+ public static void main(String[] args) {
+  
+  Item items = new Item("masterSpear",300) ;
+  
+  System.out.println(items.getWeight());
+  System.out.println(items.getName());
+  items.examine();
+    
 
 }
-	
+}
+ 
+
+ 
+
+
+ 
