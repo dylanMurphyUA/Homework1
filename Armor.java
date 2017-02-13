6739eb441056cf22f118fb19b2b5d7f88aed54f7
Unknown <dmurphy5@albany.edu>
2017-02-12 21:16:30 -0500

    Armor Class

---
+++ Armor.java
@@ -0,0 +1,17 @@
+
+public class Armor extends Item {
+	private int defense;
+	public Armor(String i, int j, int d) {
+		super(i, j);
+		defense = d;
+		
+	}
+	public int getDamage(){
+		return defense;
+	}
+	public void examine(){
+		super.examine();
+		System.out.printf("and has %d defense", defense);
+	}
+
+}
