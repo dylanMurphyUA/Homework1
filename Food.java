15cd0b7e4b75896512c9166d1d705152d2d12513
Unknown <dmurphy5@albany.edu>
2017-02-12 21:45:07 -0500

    Food class MISSING SET METHOD!

---
+++ Food.java
@@ -0,0 +1,21 @@
+
+public class Food extends Item {
+	private int nutrition;
+	private int quantity;
+	public Food(String i, int j, int d, int q) {
+		super(i, j);
+		nutrition = d;
+		quantity = q;
+		
+	}
+	public int getNutrition(){
+		return nutrition;
+	}
+	public int getQuantity(){
+		return quantity;
+	}
+	public void examine(){
+		super.examine();
+		System.out.printf("and is a food that heals %d damage.  You have %d left.", nutrition, quantity);
+	}
+}
